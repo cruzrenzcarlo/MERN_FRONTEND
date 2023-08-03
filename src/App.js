15157d3b2d7e3125f0c0e744224b392d574a84d1
cruzrenzcarlo <cruzrenzcarlo@gmail.com>
2023-08-02 18:11:22 -0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App() {
+import SimpleTodosList from "./components/simpletodolist";
+import CreateTask from "./components/createtask";
+import EditTask from "./components/edittask";
+
+
+// import Junk from "./components/junk";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+    <BrowserRouter>
+    <Routes>
+    {/* <Navbar /> */}
+
+      {/* <div className="container"> */}
+        <Route path="/" exact element={<SimpleTodosList />} />
+        <Route path="/create" element={<CreateTask />} />
+        <Route path="/update/:id" element={<EditTask />} />
+      {/* </div> */}
+      </Routes>
+    </BrowserRouter>
+    </>
   );
 }
-
-export default App;
