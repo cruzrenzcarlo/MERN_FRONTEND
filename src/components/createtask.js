1894cc6743cda4422fa52c217db0c6a2bf62e9f9
cruzrenzcarlo <cruzrenzcarlo@gmail.com>
2023-08-02 18:03:42 -0700

    first commit

---
+++ src/components/createtask.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import axios from "axios";
+import Navbar from './navbar';
+
+export default function CreateTask() {
+  const [activity, setOnChangeActivity] = useState(``);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const activityvar = { activity: activity };
+
+    axios
+      .post('https://mern-backend-w17w.onrender.com/activity/add', activityvar)
+      .then((res) => {
+        //this refreshes the page - instead can use hook use navigate
+        window.location = '/';
+        console.log('test');
+      });
+
+    console.log(activityvar);
+  };
+
+
+  return (
+    <div>
+      <Navbar />
+
+      <h3>Create New Task</h3>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label>New Task: </label>
+          {/* input onchangeactivity is the hook */}
+          <input
+            type="text"
+            required
+            className="form-control"
+            value={activity}
+            onChange={(e) => setOnChangeActivity(e.target.value)}
+          />
+        </div>
+        <br></br>
+        <div className="form-group">
+          <input
+            type="submit"
+            value="Create Activity Log"
+            className="btn btn-primary"
+          />
+        </div>
+      </form>
+    </div>
+  );
+}
